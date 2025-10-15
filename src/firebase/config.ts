9445760996b349ba265337fbe8413aaf9fa6cc50
リーバイス <samip.khanal266@gmail.com>
2025-10-15 12:11:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6734349001-f73e0",
+  "appId": "1:445476429359:web:afc6cf8cdb7e47c60170da",
+  "apiKey": "AIzaSyDsiEV-bwGG1tDtjIL9Ed6TyGEap3kOk5c",
+  "authDomain": "studio-6734349001-f73e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "445476429359"
+};
